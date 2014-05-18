101cad6645a06adf561e864097e41f805d25993b
fede.est@gmail.com <fede.est@gmail.com@28d9b1f5-9404-ee9a-b4e5-32d4479225db>
2014-05-18 22:15:24 +0000

    Siempre me falta 5 para el peso..

--- tp-orgDeDatos/Ideas/Inicial/SymbolRanking/SymbolRanking.cpp
+++ tp-orgDeDatos/Ideas/Inicial/SymbolRanking/SymbolRanking.cpp
@@ -1,29 +1,40 @@
 #include "SymbolRanking.h"
 
-SymbolRanking::SymbolRanking(){
+SymbolRanking::SymbolRanking(int ctxorden){
+	orden = ctxorden;
+}
+
+void SymbolRanking::comprimir(char* aComprimir, short* salida, unsigned long size){
+	/* Busca el contexto con un orden maximo. Si devuelve (true,x) almacenamos x en el
+	* vector y avanzamos. Si devuelve (false,x) se busca contexto de orden n-1.
+	* En caso de no ocurrencias, se itera hasta llegar a contexto 0, donde se almacena
+	* en el vector el numero ascii correspondiente al literal, mas el numero de no ocurrencias */
+
+	int ctxActual = orden;
+	char actual;
+	short numero; //Sera el numero de no ocurrencias hasta que se encuentre el simbolo.
+	for (unsigned long i = 0; i< size; i++){
+/*		actual = aComprimir[i];
+		while(ctxActual >1){
+			tuple = buscarEnContexto(ctxActual, actual, i);      // Es lo proximo a implementar. Hay que buscar una implementacion de tupla
+			numero += tuple[1];								  // e implementar buscarEnContexto.
+			if (tuple[0]) break;
+			ctxActual--;
+		}
+		if (ctxActual == 1){
+			tuple = buscarEnContextoUno(actual, i);
+			numero += tuple[1];
+			if (!tuple[0]) numero += buscarIndiceEnTabla	  // Caso de contexto = 0. Se le agrega al numero actual, el valor del char.
+		}
+		salida[i] = numero;*/
+	}
+}
+
+/* Devuelve una tupla de la forma que indica el metodo comprimir */
+void buscarEnContexto(int orden, char caracter, unsigned long pos){
 
 }
 
-SymbolRanking::SymbolRanking(unsigned char orden){
-
-}
-
-/*Recibe un vector dinamico donde se va a ir almacenando los resultados del SR y el vector a comprimir
-
-* Busca el contexto con un orden maximo. Si devuelve (true,x) almacenamos x en el
-* vector y avanzamos. Si devuelve (false,x) se busca contexto de orden n-1.
-* En caso de no ocurrencias, se itera hasta llegar a contexto 0, donde se almacena
-* en el vector el numero ascii correspondiente al literal, mas el numero de no ocurrencias
-
-*/
-void SymbolRanking::comprimir(char aComprimir[], int salida[]){
-
-}
-
-/* Dado un orden de contexto y un char busca en el vector y devuelve
-* true or false segun si lo encontro o no y el numero de no ocurrencias.
-* EJ: mejor caso (true,0) peor caso (false, contextosQueSeComparan)
-*/
-void buscarEnContexto(unsigned int orden, char caracter){
+void buscarEnContextoUno(char caracter, unsigned long pos){
 
 }
--- tp-orgDeDatos/Ideas/Inicial/SymbolRanking/SymbolRanking.h
+++ tp-orgDeDatos/Ideas/Inicial/SymbolRanking/SymbolRanking.h
@@ -4,11 +4,25 @@
 class SymbolRanking{
 	private:
 		char tabla[]; //Con los valores ASCII
-		void buscarEnContexto(unsigned char orden, char caracter);
+		int orden; //Orden maximo de contexto.
+
+		/* Dado un orden de contexto y un char busca en el vector y devuelve
+		* true or false segun si lo encontro o no y el numero de no ocurrencias.
+		* EJ: mejor caso (true,0) peor caso (false, contextosQueSeComparan)
+		*/
+		void buscarEnContexto(int orden, char caracter);
+
+		/* Realiza la busqueda del caracter para el caso de contexto = 1 */
+		void buscarEnContextoUno(char caracter, unsigned long pos);
+
 	public:
 		SymbolRanking();
-		SymbolRanking(unsigned char orden);
-		void comprimir(char aComprimir[], int salida[]);
+
+		SymbolRanking(int orden);
+
+		/*Recibe un vector de char a comprimir. El resultado se almacenara en un vector de short (necesito mas de 256 caracteres
+		 * por lo que no se puede guardar en un vector de char */
+		void comprimir(char* aComprimir, short* salida, unsigned long size);
 };
 
 #endif
